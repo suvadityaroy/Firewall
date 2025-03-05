f24baf59aebab1c8a512ea001226be211812cf15
Suvaditya Roy <143277337+suvadityaroy@users.noreply.github.com>
2025-03-06 00:28:31 +0530

    setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,8 @@
 
 from setuptools import setup, find_packages
 
-
 with open('README.rst') as f:
     readme = f.read()
-
-with open('LICENSE') as f:
-    license = f.read()
 
 setup(
     name='sample',
@@ -19,7 +15,6 @@ setup(
     author='Kenneth Reitz',
     author_email='me@kennethreitz.com',
     url='https://github.com/kennethreitz/samplemod',
-    license=license,
     packages=find_packages(exclude=('tests', 'docs'))
 )
 
